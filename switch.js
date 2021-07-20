6f7e56fa834e6dae16d8bb530e7780222343f29c
[Error:27] <gtdurst@gmail.com>
2021-07-20 16:35:48 -0400

    Fixed an issue
    Chrome users wouldn't get Coconut Mall'd

--- switch.js
+++ switch.js
@@ -1,5 +1,6 @@
 // JavaScript source code
-const memes = ["https://www.youtube.com/watch?v=QtBDL8EiNZo", "https://www.youtube.com/watch?v=fC7oUOUEEi4", "https://www.youtube.com/watch?v=6n3pFFPSlW4", "https://www.youtube.com/watch?v=QTT5iHCHSn0"];
+// Last object is a buffer so Chrome gets the last meme
+const memes = ["https://www.youtube.com/watch?v=QtBDL8EiNZo", "https://www.youtube.com/watch?v=fC7oUOUEEi4", "https://www.youtube.com/watch?v=6n3pFFPSlW4", "https://www.youtube.com/watch?v=QTT5iHCHSn0", "https://error-in-code.github.io/bait-and-switch"];
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
